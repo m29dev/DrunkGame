6cc698419eb43585316f4d6466904b4ab0ee25c3
m29dev <michalmajchrzak.it@gmail.com>
2023-11-07 13:26:57 +0100

    deployment fix

--- client/src/components/dice/DiceRoll.jsx
+++ client/src/components/dice/DiceRoll.jsx
@@ -1,16 +1,12 @@
-/* eslint-disable no-unused-vars */
 import { Button } from 'react-bootstrap'
 import './dice.css'
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { setRoomInfo } from '../../redux/authSlice'
+import { useSelector } from 'react-redux'
 import { useOutletContext } from 'react-router-dom'
 import { FaArrowAltCircleRight } from 'react-icons/fa'
 
 const DiceRoll = () => {
     const { roomInfo } = useSelector((state) => state.auth)
     const [socket] = useOutletContext()
-    const dispatch = useDispatch()
 
     const rollTheDice = () => {
         socket.emit('gameRollTheDice', { room_id: roomInfo?._id })
