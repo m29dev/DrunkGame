631b395def9564505ced3cc71438236286ebd897
m29dev <michalmajchrzak.it@gmail.com>
2023-11-11 13:02:01 +0100

    safari layout fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
     socket.auth = { userId: authInfo?.userId, user_id: authInfo?._id }
     socket.connect()
     const [gameInit, setGameInit] = useState(false)
+    const [isSafari, setIsSafari] = useState(false)
 
     useEffect(() => {
         setTimeout(() => {
@@ -27,12 +28,31 @@ function App() {
         }, 4000)
     })
 
+    useEffect(() => {
+        // Get the user-agent string
+        let userAgentString = navigator.userAgent
+        // Detect Chrome
+        let chromeAgent = userAgentString.indexOf('Chrome') > -1
+        // Detect Safari
+        let safariAgent = userAgentString.indexOf('Safari') > -1
+
+        if (chromeAgent && safariAgent) safariAgent = false
+
+        console.log('is Chrome? ', chromeAgent)
+        console.log('is Safari? ', safariAgent)
+        setIsSafari(safariAgent)
+    }, [])
+
     return (
         <>
             {!gameInit && <GameInit></GameInit>}
 
             {gameInit && (
-                <Container className="container-box">
+                <Container
+                    className={
+                        isSafari ? 'container-box safari-box' : 'container-box'
+                    }
+                >
                     <Outlet context={[socket]}></Outlet>
                 </Container>
             )}
